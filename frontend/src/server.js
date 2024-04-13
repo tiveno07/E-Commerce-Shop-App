1e3f02e9a1e2f1405ca2c1ff0e2cd10bf805b34b
Oderhohwo Tive Jude <tiveno4real@gmail.com>
2024-04-13 12:56:03 +0100

    Changed server frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2/";
+export const server = "https://e-commerce-shop-app-zeta.vercel.app/api/v2/";
 
 export const backend_url = "http://localhost:8000/";
