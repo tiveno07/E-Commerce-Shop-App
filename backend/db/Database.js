8b5425d368351aee2c64ae79e78d690bdae9ef57
Oderhohwo Tive Jude <tiveno4real@gmail.com>
2024-04-13 07:01:37 +0100

    changes in databass

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDataBase = () => {
-  mongoose.connect(process.env.MONGODB_URI).then((data) => {
-    console.log(`mongod connected with server: ${data.connection.host}`);
-  });
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`mongod connected with server: ${data.connection.host}`);
+    });
 };
 
 module.exports = connectDataBase;
