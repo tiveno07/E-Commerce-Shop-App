82b262b971c107d286c3cc04691f0f2fa41eeec8
Oderhohwo Tive Jude <tiveno4real@gmail.com>
2024-04-15 21:23:49 +0100

    server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://e-commerce-shop-app-zeta.vercel.app/api/v2";
+export const server = "https://e-commerce-shop-app-zeta.vercel.app/api/v2/";
 
 export const backend_url = "https://e-commerce-shop-app-zeta.vercel.app/";
